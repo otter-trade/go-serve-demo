4b9f6d786ca716a7958383bd9f9d335dc89a573e
dc_2 <dc_2@dcoin.com>
2023-08-24 19:26:48 +0800

    代码调整规范

--- exchange-rpc/internal/logic/admin_add_logic.go
+++ exchange-rpc/internal/logic/admin_add_logic.go
@@ -2,12 +2,11 @@ package logic
 
 import (
 	"context"
+	"github.com/otter-trade/go-serve-demo/common/i18n"
 	"github.com/otter-trade/go-serve-demo/exchange-rpc/internal/svc"
 	"github.com/otter-trade/go-serve-demo/exchange-rpc/model"
 	"github.com/otter-trade/go-serve-demo/exchange-rpc/pb"
-	"github.com/zeromicro/go-zero/core/stores/mon"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
+	"google.golang.org/grpc/status"
 	"time"
 
 	"github.com/zeromicro/go-zero/core/logx"
@@ -31,66 +30,48 @@ func NewAdminAddLogic(ctx context.Context, svcCtx *svc.ServiceContext) *AdminAdd
 func (l *AdminAddLogic) AdminAdd(in *pb.AdminAddReq) (resp *pb.AdminAddResp, err error) {
 	l.Infow("AdminAdd start", logx.Field("in", in))
 	resp = &pb.AdminAddResp{}
-
-	collection := mon.MustNewModel(l.svcCtx.Config.MongoUri.Uri, l.svcCtx.Config.MongoUri.Db, "admin")
-
-	//client, err := mongo.Connect(l.ctx, options.Client().ApplyURI(l.svcCtx.Config.MongoUri.Uri).SetMaxPoolSize(l.svcCtx.Config.MongoUri.PoolSize))
-	//if err != nil {
-	//	l.Errorw("AdminAdd", logx.Field("in", in), logx.Field("err", err))
-	//	return
-	//}
-	//defer func() {
-	//	if err = client.Disconnect(context.Background()); err != nil {
-	//		l.Errorw("AdminAdd", logx.Field("in", in), logx.Field("err", err))
-	//		return
-	//	}
-	//}()
-
-	// 选择数据库和集合
-	//collection := client.Database(l.svcCtx.Config.MongoUri.Db).Collection("admin")
 	// 插入文档
-	admin := model.Admin{
-		ID:       primitive.NewObjectID(),
+	admin := &model.Admin{
 		Name:     "John",
 		Email:    "kim@qq.com",
 		UpdateAt: time.Now(),
 		CreateAt: time.Now(),
 	}
-	_, err = collection.InsertOne(l.ctx, admin)
+	err = l.svcCtx.AdminModel.Insert(l.ctx, admin)
 	if err != nil {
 		l.Errorw("AdminAdd", logx.Field("in", in), logx.Field("err", err))
+		err = status.Error(i18n.DbError, err.Error())
 		return
 	}
 
 	// 查询文档
-	filter := bson.M{"name": "John"}
-	var result model.Admin
-	err = collection.FindOne(l.ctx, &result, filter)
+	result, err := l.svcCtx.AdminModel.FindOne(l.ctx, "64e73048a9d5231449c34f89")
 	if err != nil {
 		l.Errorw("AdminAdd", logx.Field("in", in), logx.Field("err", err))
+		err = status.Error(i18n.DbError, err.Error())
 		return
 	}
 
 	l.Infow("result", logx.Field("result", result))
 
 	// 更新文档
-	update := bson.M{"$set": bson.M{"email": "888@qq.com"}}
-
-	//objectID, err := primitive.ObjectIDFromHex(result.ID.String())
-	//if err != nil {
-	//	return nil, err
-	//}
-
-	_, err = collection.UpdateOne(l.ctx, bson.M{"_id": result.ID}, update)
+	update := &model.Admin{
+		ID:    result.ID,
+		Name:  result.Name,
+		Email: "888@qq.com",
+	}
+	_, err = l.svcCtx.AdminModel.Update(l.ctx, update)
 	if err != nil {
 		l.Errorw("AdminAdd", logx.Field("in", in), logx.Field("err", err))
+		err = status.Error(i18n.DbError, err.Error())
 		return
 	}
 
 	// 删除文档
-	_, err = collection.DeleteOne(l.ctx, filter)
+	_, err = l.svcCtx.AdminModel.Delete(l.ctx, "64e73048a9d5231449c34f89")
 	if err != nil {
 		l.Errorw("AdminAdd", logx.Field("in", in), logx.Field("err", err))
+		err = status.Error(i18n.DbError, err.Error())
 		return
 	}
 
--- exchange-rpc/internal/svc/service_context.go
+++ exchange-rpc/internal/svc/service_context.go
@@ -2,6 +2,7 @@ package svc
 
 import (
 	"github.com/otter-trade/go-serve-demo/exchange-rpc/internal/config"
+	"github.com/otter-trade/go-serve-demo/exchange-rpc/model"
 	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/core/stores/redis"
 )
@@ -9,6 +10,8 @@ import (
 type ServiceContext struct {
 	Config config.Config
 	Redis  *redis.Redis
+
+	AdminModel model.AdminModel
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
@@ -18,7 +21,8 @@ func NewServiceContext(c config.Config) *ServiceContext {
 	rds := c.RedisConf.NewRedis()
 
 	return &ServiceContext{
-		Config: c,
-		Redis:  rds,
+		Config:     c,
+		Redis:      rds,
+		AdminModel: model.NewAdminModel(c.MongoUri.Uri, c.MongoUri.Db, "admin"),
 	}
 }
